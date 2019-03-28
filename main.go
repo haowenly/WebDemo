f4a0d748a94f5aa7128a3cf79360dedc462e5419
haowenly <lihaowen2015@gmail.com>
2019-03-28 13:24:09 +0800

    test push

--- main.go
+++ main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"io"
-	"net/http"
 	"log"
+	"net/http"
 )
 
-func HelloServer(w http.ResponseWriter,req *http.Request){
-	io.WriteString(w,"hello world\n");
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello world\n")
 }
 
-func main(){
-	http.HandleFunc("/hello",HelloServer)
-	err := http.ListenAndServe(":12345",nil)
-	if err != nil{
-		log.Fatal("ListenAndServe:",err)
+func main() {
+	http.HandleFunc("/hello", HelloServer)
+	err := http.ListenAndServe(":12345", nil)
+
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
 	}
 }
